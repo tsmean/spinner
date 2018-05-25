475ddc7f61f3bbf51102b795d9489769faa1db1a
bersling <bersling@gmail.com>
2018-05-25 15:25:47 +0200

    add a unit test for the test app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from '@angular/core';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
       ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +15,23 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have some properties'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.myAnimation).toBeDefined();
+    expect(app.spinners).toBeDefined();
+    expect(app.colors).toBeDefined();
+    expect(app.sizes).toBeDefined();
+    expect(app.animationExamples).toBeDefined();
+    expect(app.sizes).toBeDefined();
+    expect(app.sizes).toBeDefined();
+    expect(app.bla).toBeUndefined();
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should have correct titles', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to spinner-library!');
+    expect(compiled.querySelector('#main-title').textContent).toContain('Angular Spinner Library');
+    expect(compiled.querySelector('#installation-instructions').textContent).toContain('Installation');
   }));
 });
